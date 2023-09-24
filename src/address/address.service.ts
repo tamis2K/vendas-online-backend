73026fe0e00313a75bd8366c3cc1405f813ed787
Mateus Varnier <mateuschioquetta@gmail.com>
2023-09-23 23:21:52 -0300

    feat: insert controller address

---
+++ src/address/address.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AddressEntity } from './entities/address.entity';
+import { Repository } from 'typeorm';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    @InjectRepository(AddressEntity)
+    private readonly addresRepository: Repository<AddressEntity>,
+  ) {}
+
+  async createAddress(
+    createAddressDto: CreateAddressDto,
+    userId: number,
+  ): Promise<AddressEntity> {
+    return this.addresRepository.save({
+      ...createAddressDto,
+      userId,
+    });
+  }
+}
--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -12,7 +12,7 @@ export class AddressEntity {
   id: number;
 
   @Column({ name: 'user_id', nullable: false })
-  userId: string;
+  userId: number;
 
   @Column({ name: 'complement', nullable: true })
   complement: string;
