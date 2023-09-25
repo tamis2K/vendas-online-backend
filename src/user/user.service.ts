8479823d857605d3b60567ebaabb2a20649dab9f
Mateus Varnier <mateuschioquetta@gmail.com>
2023-09-25 03:22:39 -0300

    feat: insert validation in emaail duplicated

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,9 @@
 import { CreateUserDto } from './dtos/createUser.dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,6 +17,14 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const user = await this.findUserByEmail(createUserDto.email).catch(
+      () => undefined,
+    );
+
+    if (user) {
+      throw new BadRequestException('email registered in system');
+    }
+
     const saltOrRounds = 10;
 
     const passwordHashed = await hash(createUserDto.password, saltOrRounds);
