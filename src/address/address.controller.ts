52a44755a7c91286a970dd47b296d02de1799309
Mateus Varnier <mateuschioquetta@gmail.com>
2023-09-25 02:31:05 -0300

    feat: create decorator userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Param,
   Post,
   UsePipes,
   ValidationPipe,
@@ -11,17 +10,18 @@ import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { Userid } from 'src/decorators/user-id.decorator';
 
 @Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post('/:userId')
+  @Post()
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddresDto: CreateAddressDto,
-    @Param('userId') userId: number,
+    @Userid() userId: number,
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddresDto, userId);
   }
---
+++ src/decorators/user-id.decorator.ts
@@ -0,0 +1,9 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { authorizationToLoginPayload } from 'src/utils/base-64-converter';
+
+export const Userid = createParamDecorator((_, ctx: ExecutionContext) => {
+  const { authorization } = ctx.switchToHttp().getRequest().headers;
+  const loginPayload = authorizationToLoginPayload(authorization);
+
+  return loginPayload?.id;
+});
