175a94a56fcbdcbc9e6aefd1b0da136662e81a44
Mateus Varnier <mateuschioquetta@gmail.com>
2023-09-29 17:10:51 -0300

    feat: create function createCategory

--- src/category/__tests__/category.service.spec.ts
+++ src/category/__tests__/category.service.spec.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { CategoryEntity } from '../entities/category.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { categoryMocK } from '../__mocks__/category.mock';
-import { async } from 'rxjs';
+import { createCategoryMock } from '../__mocks__/create-category.mock';
 
 describe('CategoryService', () => {
   let service: CategoryService;
@@ -17,6 +17,7 @@ describe('CategoryService', () => {
         {
           provide: getRepositoryToken(CategoryEntity),
           useValue: {
+            findOne: jest.fn().mockResolvedValue(categoryMocK),
             find: jest.fn().mockResolvedValue([categoryMocK]),
             save: jest.fn().mockResolvedValue(categoryMocK),
           },
@@ -35,21 +36,53 @@ describe('CategoryService', () => {
     expect(categoryRepository).toBeDefined();
   });
 
-  it('should retunr list category', async () => {
+  it('should return list category', async () => {
     const categories = await service.findAllCategories();
 
     expect(categories).toEqual([categoryMocK]);
   });
 
-  it('should retunr error in list categor empty', async () => {
+  it('should return error in list categor empty', async () => {
     jest.spyOn(categoryRepository, 'find').mockResolvedValue([]);
 
     expect(service.findAllCategories()).rejects.toThrowError();
   });
 
-  it('should retunr error in list categor exception', async () => {
+  it('should return error in list categor exception', async () => {
     jest.spyOn(categoryRepository, 'find').mockRejectedValue(new Error());
 
     expect(service.findAllCategories()).rejects.toThrowError();
+  });
+
+  it('should return error if exist category name', async () => {
+    expect(service.createCategory(createCategoryMock)).rejects.toThrowError();
+  });
+
+  it('should return category after save', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockResolvedValue(undefined);
+
+    const category = await service.createCategory(createCategoryMock);
+
+    expect(category).toEqual(categoryMocK);
+  });
+
+  it('should return error in excepttion', async () => {
+    jest.spyOn(categoryRepository, 'save').mockRejectedValue(new Error());
+
+    expect(service.createCategory(createCategoryMock)).rejects.toThrowError();
+  });
+
+  it('should return category in find by name', async () => {
+    const category = await service.findCategoryByName(createCategoryMock.name);
+
+    expect(category).toEqual(categoryMocK);
+  });
+
+  it('should return error if category find by name empty', async () => {
+    jest.spyOn(categoryRepository, 'findOne').mockResolvedValue(undefined);
+
+    expect(
+      service.findCategoryByName(categoryMocK.name),
+    ).rejects.toThrowError();
   });
 });
--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ReturnCategory } from './dtos/return-category.dto';
 import { CategoryService } from './category.service';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { CategoryEntity } from './entities/category.entity';
+import { CreateCategory } from './dtos/create-category.dto';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('category')
@@ -14,5 +23,14 @@ export class CategoryController {
     return (await this.categoryService.findAllCategories()).map(
       (category) => new ReturnCategory(category),
     );
+  }
+
+  @Roles(UserType.Admin, UserType.User)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCategory(
+    @Body() createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    return this.categoryService.createCategory(createCategory);
   }
 }
